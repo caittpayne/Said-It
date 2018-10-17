c19724bbf7008836f7dbbc666309f1bf01aac22d
Cait Payne <caitpayne@outlook.com>
2018-10-17 06:56:10 -0600

    Routing assignment complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,9 @@ module.exports = {
   init(app) {
     const staticRoutes = require('../routes/static');
     const marcoPolo = require('../routes/marco');
+    const about = require('../routes/about');
     app.use(staticRoutes);
     app.use(marcoPolo);
+    app.use(about);
   }
 }
