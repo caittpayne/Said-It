f2714492470fb9060afbbac9219dd09e17b6fdb1
Cait Payne <caitpayne@outlook.com>
2018-10-19 12:04:07 -0600

    Topic 2 checkpoint complete

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,4 +1,5 @@
 const topicQueries = require('../db/queries.topics.js');
+const { Console } = require('console');
 
 module.exports = {
   index(req, res, next) {
@@ -9,5 +10,64 @@ module.exports = {
         res.render('topics/index', {topics});
       }
     })
+  },
+
+  new(req, res, next) {
+    res.render('topics/new');
+  },
+
+  create(req, res, next) {
+    let newTopic = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    topicQueries.addTopic(newTopic, (err, topic) => {
+      if(err) {
+        console.log(err);
+        res.redirect(500, '/topics/new');
+      } else {
+        res.redirect(303, `/topics/${topic.id}`);
+      }
+    });
+  },
+
+  show(req, res, next) {
+    topicQueries.getTopic(req.params.id, (err, topic) => {
+      if(err || topic == null) {
+        res.redirect(404, '/');
+      } else {
+        res.render('topics/show', {topic});
+      }
+    });
+  },
+
+  destroy(req, res, next) {
+    topicQueries.deleteTopic(req.params.id, (err, topic) => {
+      if(err) {
+        res.redirect(500, `/topics/${topic.id}`)
+      } else {
+        res.redirect(303, '/topics')
+      }
+    });
+  },
+
+  edit(req, res, next) {
+    topicQueries.getTopic(req.params.id, (err, topic) => {
+      if(err || topic == null) {
+        res.redirect(404, '/');
+      } else {
+        res.render('topics/edit', {topic});
+      }
+    });
+  },
+
+  update(req, res, next) {
+    topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
+      if(err || topic == null) {
+        res.redirect(404, `/topics/${topic.id}`);
+      } else {
+        res.redirect(`/topics/${topic.id}`);
+      }
+    });
   }
 }
