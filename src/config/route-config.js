0df572e1318187cd29065b0fe486f9f38ca1e88f
Cait Payne <caitpayne@outlook.com>
2018-10-19 14:44:48 -0600

    Topic 2 Assignment complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,9 +4,11 @@ module.exports = {
     const marcoPolo = require('../routes/marco');
     const about = require('../routes/about');
     const topicRoutes = require('../routes/topics');
+    const advertisementRoutes = require('../routes/advertisements');
     app.use(staticRoutes);
     app.use(marcoPolo);
     app.use(about);
     app.use(topicRoutes);
+    app.use(advertisementRoutes);
   }
 }
