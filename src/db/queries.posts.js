b437af1090824f2905047bea3b5da6991c442a65
Cait Payne <caitpayne@outlook.com>
2018-11-02 16:59:01 -0600

    assignment complete

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -3,6 +3,7 @@ const Topic = require('./models').Topic;
 const Comment = require('./models').Comment;
 const User = require('./models').User;
 const Authorizer = require('../policies/post');
+const Vote = require('./models').Vote;
 const { Console } = require('console');
 
 module.exports = {
@@ -21,7 +22,7 @@ module.exports = {
         include: [
             {model: Comment, as: 'comments', include: [
                 {model: User}
-            ]}
+            ]}, {model: Vote, as: 'votes'}
         ]
     })
     .then((post) => {
